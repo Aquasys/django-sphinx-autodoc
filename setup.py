5cb4d6ef629e239030457177c6b1d35561727944
Adrien Lemaire <adrien.lemaire@aquasys.co.jp>
2011-06-11 11:02:51 +0000

    LICENSE + MANIFEST added to project

--- setup.py
+++ setup.py
@@ -1,7 +1,11 @@
 #!/usr/bin/env python
 
-import os
 from setuptools import setup, find_packages
+
+
+with open('README.rst') as file:
+        long_description = file.read()
+
 
 setup(
     name='django-sphinx-autodoc',
@@ -9,18 +13,19 @@ setup(
     author='Adrien Lemaire',
     author_email='lemaire.adrien@gmail.com',
     description='Autodoc all apps from a project to Sphinx',
-    long_description=open(os.path.join(os.path.dirname(__file__),
-                           'README.rst')).read(),
+    long_description=long_description,
     url='http://github.com/Fandekasp/django-sphinx-autodoc',
     packages=find_packages(),
     include_package_data=True,
     classifiers=[
+        "Development Status :: 3 - Alpha",
+        "License :: OSI Approved :: BSD License",
         "Framework :: Django",
         "Environment :: Web Environment",
         "Intended Audience :: Developers",
         "Intended Audience :: System Administrators",
         "Operating System :: OS Independent",
-        "Topic :: Software Development",
+        "Topic :: Software Development :: Libraries :: Python Modules",
         "Programming Language :: Python",
     ],
 )
